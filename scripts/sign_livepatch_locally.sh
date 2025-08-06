52d771a534bd543fbe82a8c364df3a34a1696379
exh0115 <165619875+exh0115@users.noreply.github.com>
2025-08-06 16:29:34 +0800

    Fix comment

--- scripts/sign_livepatch_locally.sh
+++ scripts/sign_livepatch_locally.sh
@@ -15,7 +15,7 @@ set -Eeuo pipefail
 LIVEPATCH_PRIV_KEY="secure_boot/livepatch.key"
 LIVEPATCH_PUB_KEY="secure_boot/livepatch.crt"
 if [[ ! -f "$LIVEPATCH_PRIV_KEY" || ! -f "$LIVEPATCH_PUB_KEY" ]]; then
-  echo "❌ Error: Livepatch keys not found! (sign-livepatch.sh)"
+  echo "❌ Error: Livepatch keys not found! (sign_livepatch_locally.sh)"
   echo "❌ Please run `./cvm-cli generate-livepatch-keys` and re-deploy any existing CVM so that livepatch keys will be loaded with secure boot."
   exit 1
 fi
